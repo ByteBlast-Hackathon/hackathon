e9675d6f4010711d35ddfd90b93c14547e6dfc47
igordmouraa <igordmoura_@hotmail.com>
2025-09-28 00:32:12 -0300

    feat: Adicionadas rotas para validação de token JWT e obtenção do perfil do usuário

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, UnauthorizedException, HttpCode, HttpStatus } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Controller, Post, Body, UnauthorizedException, HttpCode, HttpStatus, Get, UseGuards, Req } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import  type { Request } from 'express';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -20,7 +22,28 @@ export class AuthController {
     }
     return this.authService.login(user);
   }
+
+  @Get('validate')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.NO_CONTENT) // Retorna 204 No Content em caso de sucesso
+  @ApiOperation({ summary: 'Valida um token JWT existente de forma eficiente' })
+  @ApiResponse({ status: 204, description: 'Token é válido.' })
+  @ApiResponse({ status: 401, description: 'Não autorizado (token inválido ou em falta).' })
+  validateToken() {
+
+    return;
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Obtém o perfil completo do utilizador atualmente logado' })
+  @ApiResponse({ status: 200, description: 'Perfil do utilizador retornado com sucesso.' })
+  @ApiResponse({ status: 401, description: 'Não autorizado (token inválido ou em falta).' })
+  getProfile(@Req() req: Request) {
+
+    return req.user;
+  }
 }
-
-
 
